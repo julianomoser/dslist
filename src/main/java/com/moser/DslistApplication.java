d8e07678b1ce517835473e752bdbf14dfa43500f
Juliano Moser <juliano.moserjj@gmail.com>
2023-05-09 13:08:52 -0300

    Project created

---
+++ src/main/java/com/moser/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.moser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslistApplication.class, args);
+    }
+
+}
